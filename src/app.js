92f1eef10fee86c113b1e30f9621957c89909283
Yusron Hanan <yusronzain@gmail.com>
2020-09-25 15:30:19 +0700

    add express

---
+++ src/app.js
@@ -0,0 +1,77 @@
+const port = 3000
+const app = require('express')()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+const imaps = require('imap-simple');
+app.use(cors())
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/loginmail', upload.array(), function (req, res, next) {
+
+ var config = {
+  imap: {
+      user: req.body.email,
+      password: req.body.password,
+      host: 'imap.gmail.com',
+      port: 993,
+      tls: true,
+      tlsOptions: { rejectUnauthorized: false },
+      authTimeout: 3000
+  }
+ };
+
+
+ imaps.connect(config).then(function (connection) {
+    
+   
+    
+  return connection.openBox('INBOX').then(function () {
+      var searchCriteria = [
+          'UNSEEN'
+      ];
+ 
+      var fetchOptions = {
+          bodies: ['HEADER'],
+          markSeen: false
+      };
+ 
+      return connection.search(searchCriteria, fetchOptions).then(function (results) {
+          var subjects = results.map(function (res) {
+              return res.parts.filter(function (part) {
+                  return part.which === 'HEADER';
+              })[0].body.subject[0];
+          });
+          var dates = results.map(function (res) {
+            return res.parts.filter(function (part) {
+                return part.which === 'HEADER';
+            })[0].body.date[0];
+        });
+        var froms = results.map(function (res) {
+            return res.parts.filter(function (part) {
+                return part.which === 'HEADER';
+            })[0].body.from[0];
+        });
+ 
+        var data = {
+            subject : subjects,
+            date : dates,
+            from : froms
+        }
+
+        //   console.log(data.subject)
+        //   console.log(data);
+          res.json(data)
+      });
+  });
+ });
+  // end here
+
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
+const express_node = require('./app')
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -11,18 +11,19 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-  });
+});
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
